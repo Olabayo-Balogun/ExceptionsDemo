88f4d6ed538df8ab4ef236ca4b1d95e858651601
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-05-11 16:34:53 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,61 @@
+﻿using ExceptionsLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //When you type the word "DemoCode" it gives you the ability to link to class libraries that you haven't already linked.
+
+            //The code just below instantiates demo code
+            DemoCode demo = new DemoCode();
+
+            //Recall from the ExceptionsLibary class, that when we want to call any of the method in it, we need to specify a number as a parameter
+            //The number refers to the position in an array that we created
+            //When we input the number parameter when calling the method it'll return to us the item in that particular position in the array we created
+            //Simply put, by writing "3" in the "demo.GrandParentMethod(3);" we're requesting for the fourth item in the array we created in the ExceptionsLibrary class.
+            //Arrays start counting from position 0 (which is the first item) as such 3 is the fourth position if you follow the n+1 principle.
+            //int result = demo.GrandParentMethod(3);
+
+            //When we specify a unit parameter that is beyond what the length of the array you get an out of bounds exception.
+            //Note that variables created within blocks (curly braces) are limited in scope to that block.
+            //If you need to be able to access the variable outside the block you must first declare the variable globally (outside the block).
+            try
+            {
+                int result = demo.GrandParentMethod(4 );
+                Console.WriteLine($"The value at the given position is {result}");
+            }
+            //Note that you can catch multiple types of error within one try catch usage.
+            //Make sure the "Exception" is the last exception type to catch because it's very general
+            //Note that the parameter "ex" is a variable parameter that can be used repeatedly because it is limited in scope to the catch block it is mentioned in.
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("You gave us suspicious information, are you a hoodlum?");
+            }
+            catch (Exception ex)
+            {
+                //Try catch should be done as high as possible in the program so as to ensure that you get as much information as possible about the root of the problem and every other place that is affected by the problem.
+                //Low level classes don't need try catch as much as high level classes.
+                //If you don't have a throw here it means the application can continue to run in spite of the error.
+                Console.WriteLine(ex.Message);
+                //The line of code below shows you the stack trace
+                Console.WriteLine(ex.StackTrace);
+
+                //The codes below help get the inner exception (which is the rest of the stack trace)
+                //As long as there's still an inner exception it will write out the stack trace.
+                var inner = ex.InnerException;
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        //What the code below does is illustrate and drive home the point that you shouldn't call methods directly, a bigger method should call a bunch of smaller methods rather than calling a method that it needs directly.
+        public int GrandParentMethod(int position)
+        {
+            int output = 0;
+            Console.WriteLine("Open database connection" );
+
+            try
+            {
+                output = ParentMethod(position);
+
+                //If you want, you can catch exceptions within the "try" block 
+                //Note that this isn't the best way to handle errors like these
+                //Volunteers relating to input can be done within the input validation
+                //Code
+                //if (position < 0)
+                //{
+                //    throw new IndexOutOfRangeException("The value has to be greater than or equal to zero");
+                //}
+            }
+            catch (Exception ex)
+            {
+                //Throw is used to catch errors and pass it up to the higher class/code
+                //It's especially useful when you want to create exception handling in the lower classes
+                //throw can also be used to log errors.
+                //in average cases, "throw;" is enough
+                //Don't write "throw ex" because it's not the same as "throw", it shortens the stack trace and doesn't show the full stack trace outlining all the classes and methods affected by the project.
+                //Don't write "throw new Exception("Bespoke exception message")" because it also reduces the stack trace.
+
+                //You should throw a new exception when it is specific to an exception type you want to catch
+                //When you type "throw new Exception" you get a list of suggestions
+                //Throwing a new exception also reduces the stack trace, the way to get around it is when you add an inner exception.
+                //The code below shows you the format of an inner exception
+                //The new exception type you created will respond to the exception in question, however, the inner exception will show the trigger/origin of the new exception type thus giving you the full stack trace
+                throw new ArgumentException("You passed bad data", ex);
+            }
+            //Code enclosed in the "finaly" will run no matter what
+            //You must not put code that has bugs or that will throw an exception in the "finally" block
+            finally
+            {
+                Console.WriteLine("Close database connection");
+            }
+
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        //It's not advisable to use your try catch in this level of the program, it should preferably be used in the user interface side.
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            //try
+            //{
+                int[] numbers = new int[] { 1, 4, 7, 2 };
+                output = numbers[position];
+            //}
+
+            //What the code below does is that it catches every exception and logs, it's a bit too general and not specific enough
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    //The line of code belows tells you exactly where the error happened
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+            return output;
+        }
+    }
+}
